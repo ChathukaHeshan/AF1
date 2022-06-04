b0497bf62974b8b4eca9ef02c945bc266d47f3de
DESKTOP-ERETTBS\Chathuka Heshan <chathukaheshan@gmail.com>
2022-06-04 12:02:13 +0530

    First Commit8

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI,{
