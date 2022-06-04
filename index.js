7a3f67d765dcf4d198887e7a3d6ff015d8de7ab5
DESKTOP-ERETTBS\Chathuka Heshan <chathukaheshan@gmail.com>
2022-06-04 13:01:53 +0530

    commit23

--- index.js
+++ index.js
@@ -57,12 +57,15 @@ app.use('/api/template', templateRoute());
 
 app.use('/api/uploads', express.static('uploads'));
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    });
-   }
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port ${PORT}`);
